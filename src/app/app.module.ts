cdb192eeaf510df47d96a9dc1ecc228b1b5fd390
strannik <strannik.anton@gmail.com>
2017-02-20 00:10:52 +0400

    Add firebase integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from "angularfire2";
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQNRIJboG04NYmD-VxJQHBppB7RIVLWrM",
+  authDomain: "angularcoursework.firebaseapp.com",
+  databaseURL: "https://angularcoursework.firebaseio.com",
+  storageBucket: "angularcoursework.appspot.com",
+  messagingSenderId: "602127487335"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
